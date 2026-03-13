8298d26849d7dfad194c4cfefb4e95c673641aed
Aldren Letada <aldren.l@pandolink.com>
2026-03-13 13:09:22 +0800

    fix firebase build prod

--- firebase/configFirebase.tsx
+++ firebase/configFirebase.tsx
@@ -1,7 +1,8 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database"; // Import Realtime Database
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +18,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
 
 // Export Firebase services
 export const auth = getAuth(app);
